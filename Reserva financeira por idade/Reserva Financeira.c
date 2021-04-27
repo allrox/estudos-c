acff62a02fa68dbc15d527d728d3a218bb850f7f
allrox <alexandre@allrox.com.br>
2021-04-27 00:07:57 -0300

    Envio do estudo

--- Reserva financeira por idade/Reserva Financeira.c
+++ Reserva financeira por idade/Reserva Financeira.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main(void){
+int main(){
 
   system("cls");
   setlocale(LC_ALL,"Portuguese_Brazil");
 
   int idadeAtual;
-  float rendaBruta, patrimonioIdeal;
+  float rendaBruta;
 
   printf("Esta aplicação calcula o patrimônio ideal para a sua idade,\nseguindo o método de Thomas J. Stanley e William D. Danko.\n\n");
   
@@ -19,7 +19,7 @@ int main(void){
   scanf("%f", &rendaBruta);
   printf("\n");
 
-  patrimonioIdeal = ( ( (float) idadeAtual * rendaBruta ) / 10 );
+  float patrimonioIdeal = ( ( (float) idadeAtual * rendaBruta ) / 10 );
 
   printf("O patrimônio ideal de acordo com a idade informada é igual a R$ %.2f\n", &patrimonioIdeal);
 
